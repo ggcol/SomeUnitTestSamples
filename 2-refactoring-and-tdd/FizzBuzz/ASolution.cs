3af1ddbd831c30d770ea326c18423d5654f8146d
Gianluca Colombo <gianluca.colombo@lobra.com>
2024-07-03 09:25:58 +0200

    fixed couple of tdd comments

--- 2-refactoring-and-tdd/FizzBuzz/ASolution.cs
+++ 2-refactoring-and-tdd/FizzBuzz/ASolution.cs
@@ -4,6 +4,7 @@ public class ASolution : ISolution
 {
     public IEnumerable<string> FizzBuzz(int n)
     {
+        //implement during demo
         throw new NotImplementedException();
     }
 }
--- 2-refactoring-and-tdd/FizzBuzz/ISolution.cs
+++ 2-refactoring-and-tdd/FizzBuzz/ISolution.cs
@@ -3,7 +3,7 @@
 public interface ISolution
 {
     /*
-     * Given an integer n, return a string array answer (1-indexed) where:
+     * Given an integer n, return a string array "answer" (1-indexed) where:
      * answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
      * answer[i] == "Fizz" if i is divisible by 3.
      * answer[i] == "Buzz" if i is divisible by 5.
